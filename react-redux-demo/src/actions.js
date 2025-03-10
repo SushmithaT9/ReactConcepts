1af4cc2c008cd242aa89db2d117321afe9be6e58
unknown <sushmithat963@gmail.com>
2025-03-10 15:24:25 +0530

    Updated ReactConcepts folder

---
+++ react-redux-demo/src/actions.js
@@ -0,0 +1,13 @@
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+// Action creators
+export const increment = () => ({
+  type: INCREMENT,
+});
+
+export const decrement = () => ({
+  type: DECREMENT,
+});
+
+
